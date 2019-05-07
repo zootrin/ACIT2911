f502976bd66bde13a1140adc35f205c6a527124d
Cathy Chan <chan.cathy@hotmail.ca>
2019-05-07 15:09:38 -0700

    atlas rebuild

--- utils.js
+++ utils.js
@@ -3,6 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 var _dbUser = null;
 
 var CONNECTION_URL = "mongodb+srv://example:asdfasdf@agiledevelopment-1ppyn.mongodb.net/test?retryWrites=true";
+var DATABASE = 'forumdb';
 
 getDb = () => {
     return _dbUser;
@@ -17,7 +18,7 @@ init = (callback) => {
         if (err) {
             return console.log('Unable to connect to DB');
         }
-        _dbUser = client.db("forumdb");
+        _dbUser = client.db(DATABSE);
         console.log('Successfully connected to MongoDB server');
     });
 };
